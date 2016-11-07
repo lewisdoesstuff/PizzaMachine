a2c4d46c1c08c7b82d239ca6fb0fbbb66398b7f1
DeathByKorea <lewiszvvz@gmail.com>
2016-11-07 22:06:06 +0000

    -Added Files

--- Pizza Machine/Pizza Machine/Module1.vb
+++ Pizza Machine/Pizza Machine/Module1.vb
@@ -1,213 +1,134 @@
-﻿'Lewis Wood
-Imports System.Text ' system.text for getting .length (don't know why this isn't native but whatever)
+﻿'{Program Property Of Lewis Wood ( For Reals This Time ) }
+
+Imports System.Text 'system.text to find length of var
 Module Module1
     Dim name As String
     Dim address As String
-    Dim phonenumber As String
-    Dim pizza(19) As String
-    Dim pizzaprice(19) As String ' all my variables and arrays, mostly as strings becuase isnumeric()
-    Dim pizzaamount As Integer
-    Dim cost As Decimal
-    Dim payment As String
-    Dim cnumber As String
-    Dim code As String
-    Dim stringammount As String
+    Dim pnum As String
+    Dim amount As String
+    Dim pamount As UInteger 'declarations
+    Dim pizzafinal(19) As String
+    Dim pprice(19) As String
+    Dim cost As UInteger
     Dim menulist = {"1.Cheese & Tomato – Italian-style six-cheese blend – £7.50",
 "2.BBQ Chicken – Chargrilled chicken, barbeque sauce, bacon, onions – £7.90",
 "3.Meat Feast – Ham, peperoni, sausage, bacon, spicy beef – £8.10",
-"4.Piri-Piri Chicken – Chilli pepper sauce, chargrilled chicken – £8.80",
+"4.Piri-Piri Chicken – Chilli pepper sauce, chargrilled chicken – £8.80", ' menu in array
 "5.Hawaii – Ham, pineapple, mushrooms – £8.90",
 "6.Mediterranean – Chorizo, Italian-style sausage, jalapeno sausage – £9.50",
 "7.The Mexican – Jalapeno peppers, red peppers, spicy beef, onions – £9.70",
-"8.The Works – Pepperoni, sausage, ham, mushrooms, green peppers – £9.90"} ' store all the pizzas in an array
+"8.The Works – Pepperoni, sausage, ham, mushrooms, green peppers – £9.90"}
     Sub Main()
-        Console.ForegroundColor = ConsoleColor.Cyan
-        Console.WriteLine("Hello and welcome to Jimmys Pizza")
+        Console.WriteLine("Pizza App v2")
         getname()
         getaddress()
-        getphonenumber() ' calling all the functions
-        menu()
-        calc()
-        total()
-        pay()
+        getpnum() ' calling functions ( makes the program modular and easier to read/debug imo
+        getorder()
+        math()
         recipt()
-    End Sub
-    Function getname()
-        Console.ForegroundColor = ConsoleColor.Yellow
-        ' Console.TreatControlCAsInput = True ' ~~stops people ^C-ing to crash it~~ okay, so it turns out that microsoft haven't fixed a bug from 2006 which breaks loops if this is true. 10 YEARS GUYS.
 
-        Console.WriteLine("May i take your name?")
-        name = Console.ReadLine() ' store name in name variable
-        Return True ' returns true becuase you can't really mess up typing a bunch of letters
+    End Sub
+    Public Function getname()
+        Console.WriteLine("Please enter your name")
+        name = Console.ReadLine() ' get name
         Console.Clear()
-    End Function
-    Function getaddress()
-        Console.ForegroundColor = ConsoleColor.Green
-        Console.WriteLine("May i take your address?") ' same thing with address
-        address = Console.ReadLine()
         Return True
     End Function
-    Function getphonenumber()
-        Console.ForegroundColor = ConsoleColor.Magenta
+    Public Function getaddress()
+        Console.WriteLine("Please enter your address")
+        address = Console.ReadLine()
         Console.Clear()
-        Console.WriteLine("May i take your phone number? Please include the area code if it is a landline")
-        phonenumber = Console.ReadLine()
-        If IsNumeric(phonenumber) AndAlso phonenumber.Length > 10 And phonenumber.Length < 13 Then ' checks if the phonenumber is 11 or 12 digits and is a numeric value
+        Return True ' same as name but with address var
+    End Function
+    Public Function getpnum()
+        Console.WriteLine("Please enter your phone number (inc area code)")
+        pnum = Console.ReadLine()
+        If IsNumeric(pnum) AndAlso (pnum.Length >= 11 AndAlso pnum.Length <= 12) Then ' same as before, check numericalicy(probably not a word) and length to be 11 or 12
             Return True
         Else
-            Console.ForegroundColor = ConsoleColor.Red
-            Console.WriteLine("Incorrect Phone Number, Try Again.")
-            getphonenumber()
+            Console.WriteLine("Incorrect entry, check it's a numerical value and its the correct length (11-12 digits)")
+            Threading.Thread.Sleep(2000)
+            getpnum() ' error!
             Return False
         End If
     End Function
-    Function menu()
-        Console.Clear()
-        Console.ForegroundColor = ConsoleColor.DarkCyan
+    Public Function getorder()
         For Each line As String In menulist
-            Console.WriteLine(line) ' prints the menu
+            Console.WriteLine(line) ' print menu
         Next
-        Console.ForegroundColor = ConsoleColor.Blue
-        Console.WriteLine("Please enter how many pizzas you would like to order") ' gets how many pizzas they want
-        stringammount = Console.ReadLine()
-        If IsNumeric(stringammount) Then
-            pizzaamount = stringammount
+        Console.WriteLine("How many pizzas do you want?")
+        amount = Console.ReadLine() ' get order amount
+        If IsNumeric(amount) AndAlso amount <= 20 Then ' check numericalicy and length
+            pamount = amount
         Else
-            Console.ForegroundColor = ConsoleColor.Red
-            Console.WriteLine("Incorrect value")
-            Threading.Thread.Sleep(2000)
-            menu()
+            Console.WriteLine("Incorrect value, check for numeric value and that it's less than 20") ' Error!
         End If
-        If pizzaamount <= 20 Then ' check that its less than/equal to 20
-        Else
-            Console.ForegroundColor = ConsoleColor.Red
-            Console.WriteLine("Incorrect value")
-            Threading.Thread.Sleep(2000)
-            menu()
-        End If
-        Console.ForegroundColor = ConsoleColor.Yellow
-        Console.WriteLine("Enter your pizzas:")
-        pizzaamount = pizzaamount - 1
-        For i As Integer = 0 To pizzaamount ' gets the pizzas and stores them in pizza(19)
-            pizza(i) = Console.ReadLine
-            If pizza(i) > 8 Or IsNumeric(pizza(i)) Then
+        Console.WriteLine("Enter your pizzas")
+        pamount = (pamount - 1) ' do some math!
+        For i As Integer = 0 To pamount
+            pizzafinal(i) = Console.ReadLine() ' get pizza input
+            If pizzafinal(i) > 8 Or IsNumeric(pizzafinal(i)) Then
             Else
-                Console.ForegroundColor = ConsoleColor.Red
-                Console.WriteLine("Incorect value")
+                Console.WriteLine("Incorrect value, check it's not bigger than 8 and that it's numerical") ' again same as before
                 Threading.Thread.Sleep(2000)
-                menu()
+                getorder()
             End If
         Next
         Return True
     End Function
-    Function calc()
-        Console.Clear()
-
-        Dim potato As Integer
-        For potato = 0 To pizzaamount ' probably inefficiant but it works
-            If pizza(potato) = 1 Then ' if its 1, then put price to 7.5 and add the name of the pizza to the array
-                pizzaprice(potato) = 7.5
-                pizza(potato) = "Cheese & Tomato – Italian-style six-cheese blend"
-            ElseIf pizza(potato) = 2 Then
-                pizzaprice(potato) = 7.9
-                pizza(potato) = "BBQ Chicken – Chargrilled chicken, barbeque sauce, bacon, onions"
-            ElseIf pizza(potato) = 3 Then
-                pizzaprice(potato) = 8.1
-                pizza(potato) = "Meat Feast – Ham, peperoni, sausage, bacon, spicy beef"
-            ElseIf pizza(potato) = 4 Then
-                pizzaprice(potato) = 8.8
-                pizza(potato) = "Piri-Piri Chicken – Chilli pepper sauce, chargrilled chicken"
-            ElseIf pizza(potato) = 5 Then
-                pizzaprice(potato) = 7.9
-                pizza(potato) = "Hawaii – Ham, pineapple, mushrooms"
-            ElseIf pizza(potato) = 6 Then
-                pizzaprice(potato) = 7.9
-                pizzaprice(potato) = "Mediterranean – Chorizo, Italian-style sausage, jalapeno sausage"
-            ElseIf pizza(potato) = 7 Then
-                pizzaprice(potato) = 7.9
-                pizza(potato) = "The Mexican – Jalapeno peppers, red peppers, spicy beef, onions"
-            ElseIf pizza(potato) = 8 Then
-                pizzaprice(potato) = 7.9
-                pizza(potato) = "The Works – Pepperoni, sausage, ham, mushrooms, green peppers"
+    Public Function math()
+        Dim mathint As Integer
+        For mathint = 0 To pamount
+            If pizzafinal(mathint) = 1 Then
+                pprice(mathint) = 7.5 ' huge ifElseif, probably could use a loop, too late at night for that.
+                pizzafinal(mathint) = "Cheese & Tomato – Italian-style six-cheese blend"
+            ElseIf pizzafinal(mathint) = 2 Then
+                pprice(mathint) = 7.9
+                pizzafinal(mathint) = "BBQ Chicken – Chargrilled chicken, barbeque sauce, bacon, onions"
+            ElseIf pizzafinal(mathint) = 3 Then
+                pprice(mathint) = 8.1
+                pizzafinal(mathint) = "Meat Feast – Ham, peperoni, sausage, bacon, spicy beef"
+            ElseIf pizzafinal(mathint) = 4 Then
+                pprice(mathint) = 8.8
+                pizzafinal(mathint) = "Piri-Piri Chicken – Chilli pepper sauce, chargrilled chicken"
+            ElseIf pizzafinal(mathint) = 5 Then
+                pprice(mathint) = 7.9
+                pizzafinal(mathint) = "Hawaii – Ham, pineapple, mushrooms"
+            ElseIf pizzafinal(mathint) = 6 Then
+                pprice(mathint) = 7.9
+                pprice(mathint) = "Mediterranean – Chorizo, Italian-style sausage, jalapeno sausage"
+            ElseIf pizzafinal(mathint) = 7 Then
+                pprice(mathint) = 7.9
+                pizzafinal(mathint) = "The Mexican – Jalapeno peppers, red peppers, spicy beef, onions"
+            ElseIf pizzafinal(mathint) = 8 Then
+                pprice(mathint) = 7.9
+                pizzafinal(mathint) = "The Works – Pepperoni, sausage, ham, mushrooms, green peppers"
             End If
         Next
-        For potato = 0 To pizzaamount
-            cost = cost + pizzaprice(potato) ' add all the costs
+        For mathint = 0 To pamount
+            cost = cost + pprice(mathint) ' calculate the cost
+
         Next
         If cost > 20 Then
-            cost = cost - (cost * 0.2)
+            cost = cost - (cost * 0.2) ' add discount (subtract?)
+
         End If
         If cost < 10 Then
-            cost = cost + 1.5
-        End If
-        Return True
-    End Function
-    Function total()
-        Console.ForegroundColor = ConsoleColor.Cyan
-        Math.Round(cost, 2, MidpointRounding.AwayFromZero) ' round the number incase of decimal madness
-        Console.WriteLine("The cost of the pizza is £" & cost)
-        Return True
-    End Function
-    Function pay()
-        Console.ForegroundColor = ConsoleColor.Blue
-        Console.WriteLine("How would you like to pay?")
-        Console.WriteLine("")
-        Console.WriteLine("1. Card") ' get payment method
-        Console.WriteLine("2. On Delivery")
-        payment = Console.ReadLine
-        If payment = 2 Then
-            Console.ForegroundColor = ConsoleColor.Green
-            Console.WriteLine("Please have £" & cost & " ready at the door")
-            Threading.Thread.Sleep(2000)
-        ElseIf payment = 1 Then
-            Console.ForegroundColor = ConsoleColor.White
-            Console.WriteLine("Please enter your card number (16 digit)")
-            cnumber = Console.ReadLine()
-            Console.ForegroundColor = ConsoleColor.Green
-            If IsNumeric(cnumber) AndAlso cnumber.Length = 16 Then 'check for card length and numericness
-                Console.WriteLine("Please enter the 3 digit CVC number (Found on the back)") ' get CVC code
-                code = Console.ReadLine()
-                If IsNumeric(code) AndAlso code.Length = 3 Then ' check length and numericness
-                    Return True
-                Else
-                    Console.ForegroundColor = ConsoleColor.Red
-                    Console.WriteLine("That was incorrect, Please try again")
-                    Threading.Thread.Sleep(2000)
-                    pay()
-                    Return False
-                End If ' tell them they're wrong
-            Else
-                Console.ForegroundColor = ConsoleColor.Red
-                Console.WriteLine("That was incorrect, Please try again")
-                Threading.Thread.Sleep(2000)
-                pay()
-                Return False
-            End If
+            cost = cost + 1.5 ' delivery charge
+
         End If
         Return True
     End Function
     Function recipt()
-        Console.ForegroundColor = ConsoleColor.Green
         Console.Clear()
-        Console.WriteLine("~~~~~~~~~~~~~~~RECIPT~~~~~~~~~~~~~~~")
-        Console.WriteLine("")
-        For i As Integer = 0 To pizzaamount ' print the recipt and all their pizzas
-            Console.WriteLine(pizza(i))
+        Console.WriteLine("**********RECIPT**********")
+        Console.WriteLine()
+        For i As Integer = 0 To pamount
+            Console.WriteLine(pizzafinal(i)) ' print the pizzas ordered nicely
+
         Next
-        Console.ForegroundColor = ConsoleColor.Cyan
-        Console.WriteLine("Order Cost:              £" & cost)
-        Console.WriteLine("Order Number:           " & GenerateRandomString(6, False)) ' get a random number, 6 characters with no uppercase
+        Console.WriteLine("Order Subtotal            £" & cost) ' print cost
         Console.ReadLine()
         Return True
-    End Function
-
-    Public Function GenerateRandomString(ByRef len As Integer, ByRef upper As Boolean) As String
-        Dim rand As New Random()
-        Dim allowableChars() As Char = "ABCDEFGHIJKLOMNOPQRSTUVWXYZ0123456789".ToCharArray()
-        Dim final As String = String.Empty
-        For i As Integer = 0 To len - 1 ' random number generator courtesy of stackoverflow
-            final += allowableChars(rand.Next(allowableChars.Length - 1))
-        Next
-        Return IIf(upper, final.ToUpper(), final)
     End Function
 End Module
